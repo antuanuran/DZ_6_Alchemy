125318686511a94b200dcf6afbf68b8572fea884
test <test@neto.com>
2023-02-16 07:46:22 +0300

    Результат

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import sessionmaker
-from Models import create_db, Publisher, Book, Shop, Sale, Stock
+from models import create_db, Publisher, Book, Shop, Stock, Sale
 
 DSN = 'postgresql://postgres:postgres@localhost:5432/Home_6'
 engine = sq.create_engine(DSN)
@@ -18,46 +18,40 @@ pub2 = Publisher(name='Булгаков')
 pub3 = Publisher(name='Лермонтов')
 pub4 = Publisher(name='Пушкин')
 
-
 # 2/Создаем экземпляры книг
 book1 = Book(title='Война и мир', bind_publisher=pub1)
-book2 = Book(title='Мастер и Маргарита', bind_publisher=pub2)
+book2 = Book(title='Мастер и маргарита', bind_publisher=pub2)
 book3 = Book(title='Анна Каренина', bind_publisher=pub1)
 book4 = Book(title='Бородино', bind_publisher=pub3)
-book5 = Book(title='У лукоморья', bind_publisher=pub4)
+book5 = Book(title='У Лукоморья', bind_publisher=pub4)
 book6 = Book(title='Узник', bind_publisher=pub4)
-
-
 
 # 3/Создаем экземпляры магазинов
 shop1 = Shop(name='Цум')
 shop2 = Shop(name='Литератор')
 shop3 = Shop(name='Магазин книг')
 
-
 # 4/Создаем экземпляры Стоков
 stock1 = Stock(count=20, bind_book=book1, bind_shop=shop1)
-stock2 = Stock(count=5, bind_book=book2, bind_shop=shop2)
-stock3 = Stock(count=70, bind_book=book3, bind_shop=shop3)
-stock4 = Stock(count=15, bind_book=book4, bind_shop=shop2)
-stock5 = Stock(count=25, bind_book=book5, bind_shop=shop1)
+stock2 = Stock(count=12, bind_book=book2, bind_shop=shop2)
+stock3 = Stock(count=6, bind_book=book3, bind_shop=shop3)
+stock4 = Stock(count=70, bind_book=book4, bind_shop=shop2)
+stock5 = Stock(count=28, bind_book=book5, bind_shop=shop1)
 stock6 = Stock(count=100, bind_book=book6, bind_shop=shop3)
 
-
-
-# 5/Создаем экземпляры продаж
-sale1 = Sale(price='15 000', date_sale='12.10.2022', count=25, bind_stock=stock1)
-sale2 = Sale(price='20 000', date_sale='15.10.2021', count=10, bind_stock=stock2)
-sale3 = Sale(price='15 000', date_sale='12.11.2022', count=20, bind_stock=stock3)
-sale4 = Sale(price='18 000', date_sale='16.10.2021', count=18, bind_stock=stock4)
-sale5 = Sale(price='21 000', date_sale='18.09.2022', count=12, bind_stock=stock5)
-sale6 = Sale(price='22 000', date_sale='10.11.2021', count=16, bind_stock=stock6)
-sale7 = Sale(price='26 000', date_sale='12.10.2022', count=24, bind_stock=stock5)
-sale8 = Sale(price='13 000', date_sale='30.10.2021', count=10, bind_stock=stock4)
+# 5/Создаем экземпляры Распродаж
+sale1 = Sale(price='15 000 р.', date_sale='12.10.2022', count=25, bind_stock=stock1)
+sale2 = Sale(price='25 000 р.', date_sale='12.11.2022', count=10, bind_stock=stock2)
+sale3 = Sale(price='35 000 р.', date_sale='12.05.2022', count=12, bind_stock=stock3)
+sale4 = Sale(price='17 000 р.', date_sale='15.03.2022', count=16, bind_stock=stock4)
+sale5 = Sale(price='19 000 р.', date_sale='12.02.2023', count=18, bind_stock=stock5)
+sale6 = Sale(price='12 000 р.', date_sale='12.01.2023', count=31, bind_stock=stock6)
+sale7 = Sale(price='21 000 р.', date_sale='12.10.2022', count=33, bind_stock=stock5)
+sale8 = Sale(price='23 000 р.', date_sale='02.12.2022', count=41, bind_stock=stock4)
 
 
 # Добавляем и закрываем сессию
-sess.add_all([pub1, pub2, pub2, pub4])
+sess.add_all([pub1, pub2, pub3, pub4])
 sess.add_all([book1, book2, book3, book4, book5, book6])
 sess.add_all([shop1, shop2, shop3])
 sess.add_all([stock1, stock2, stock3, stock4, stock5, stock6])
@@ -67,32 +61,34 @@ sess.commit()
 
 
 
-# Запросы
+# Запросы:
 
-# 1/Запрос
-question1 = input("Введите  Писателя:")
-print(f'\nВсе книги писателя по фамилии {question1}:')
-for a in sess.query(Book).join(Publisher).filter(Publisher.name == question1):
-    print(a)
+# 1/ Запрос. Вывести все книги писателя по его Фамилии
+question1 = input('1.Введите фамилию писателя:')
+print(f'\nВсе книги писателя по фамилии: {question1}:')
+for x in sess.query(Book).join(Publisher).filter(Publisher.name == question1):
+    print(f'| {x.title:15} |')
 
-# 2/Запрос
-question2 = input("\nВведите название Магазина:")
-print(f'\nВсе книги, проданные в магазине: {question2}:')
-for b in sess.query(Book).join(Publisher).join(Stock).join(Shop).filter(Shop.name == question2):
-    print(b)
 
-# 3/Запрос
-question3 = input("\nВведите  Писателя:")
-print(f'\nВсе покупки книг, написанные писателем: {question3}:')
-for c in sess.query(Sale).join(Stock).join(Book).join(Publisher).filter(Publisher.name == question3):
-    print(c)
+# 2/ Запрос. Ввести название магазина и вывести все книги, которые есть в этом магазине
+question2 = input('\n2. Введите название магазина:')
+print(f'\nВсе книги, которые есть в магазине: {question2}:')
+for x in sess.query(Book).join(Stock).join(Shop).filter(Shop.name == question2):
+    print(f'| {x.title:15} |')
 
-# 4/Запрос
-question3 = input("\nВведите  Писателя:")
-print(f'\nВсе магазины, где продаются книги писателя: {question3}:')
-for d in sess.query(Shop).join(Stock).join(Sale).join(Book).join(Publisher).filter(Publisher.name == question3):
-    print(d)
 
+# 3/ Запрос. Ввести фамилию писателя и вывести все магазины, где продаются книги данного писателя
+question4 = input('\n3. Введите фамилию писателя:')
+print(f'\nВсе магазины, где продаются книги писателя по фамилии: {question4}:')
+for x in sess.query(Shop).join(Stock).join(Book).join(Publisher).filter(Publisher.name == question4):
+    print(f'| {x.name:15} |')
+
+
+# 4/ Запрос. Ввести фамилию писателя и вывести: название книги, магазин, кол-во проданных книг, стоимость и дату продажи
+question4 = input('\n4. Введите фамилию писателя:')
+print(f'\nВся информация по продажам писателя по фамилии: {question4}:')
+for x in sess.query(Sale).join(Stock).join(Book).join(Publisher).filter(Publisher.name == question4):
+    print(f'| {x.bind_stock.bind_book.title:15} | {x.bind_stock.bind_shop.name:18} | {x.count:3} | {x.price:10} | {x.date_sale:10} |')
 
 
 
--- Models.py
+++ Models.py
@@ -3,23 +3,20 @@ from sqlalchemy.orm import declarative_base, relationship
 
 BaseModel = declarative_base()
 
+
 class Publisher (BaseModel):
     __tablename__ = 'publisher'
 
     id = sq.Column(sq.BigInteger(), primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
 
-    def __str__(self):
-        return f'{self.name}'
-
-
 class Book (BaseModel):
     __tablename__ = 'book'
 
     id = sq.Column(sq.BigInteger(), primary_key=True)
     title = sq.Column(sq.String(length=60))
-    id_publisher = sq.Column(sq.BigInteger(), sq.ForeignKey('publisher.id'))
 
+    id_publisher = sq.Column(sq.BigInteger(), sq.ForeignKey('publisher.id'))
     bind_publisher = relationship(Publisher)
 
     def __str__(self):
@@ -35,20 +32,16 @@ class Shop (BaseModel):
     def __str__(self):
         return f'{self.name}'
 
-
 class Stock (BaseModel):
     __tablename__ = 'stock'
 
     id = sq.Column(sq.BigInteger(), primary_key=True)
     count = sq.Column(sq.BigInteger())
+
     id_book = sq.Column(sq.BigInteger(), sq.ForeignKey('book.id'))
     id_shop = sq.Column(sq.BigInteger(), sq.ForeignKey('shop.id'))
-
     bind_book = relationship(Book)
     bind_shop = relationship(Shop)
-
-    def __str__(self):
-        return f'{self.id_shop}'
 
 
 
@@ -59,55 +52,16 @@ class Sale (BaseModel):
     price = sq.Column(sq.String(length=60))
     date_sale = sq.Column(sq.String(length=20))
     count = sq.Column(sq.BigInteger())
-    id_stock = sq.Column(sq.BigInteger(), sq.ForeignKey('stock.id'))
 
+    id_stock = sq.Column(sq.BigInteger(), sq.ForeignKey('stock.id'))
     bind_stock = relationship(Stock)
 
     def __str__(self):
-        return f'{self.id_stock} | {self.price} | {self.date_sale} | {self.count}'
+        return f'{self.id}'
 
 
 
 def create_db(engine):
     BaseModel.metadata.drop_all(engine)
     BaseModel.metadata.create_all(engine)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
